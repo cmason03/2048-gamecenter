183005baa90cdf081e0d31ee72353a9308e22a5a
cmason03 <corey.mason@tufts.edu>
2014-03-28 18:29:05 -0400

    testing sending a score

---
+++ web.js
@@ -0,0 +1,20 @@
+
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
+
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/scoreboard',function (req,res){
+	res.send(LocalStorageManager.prototype.getBestScore())
+});
+
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
